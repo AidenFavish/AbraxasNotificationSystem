69464613b457f343b39f491c61968e2fefdbbcf2
Aiden F <aidenfavish@gmail.com>
2022-12-05 19:27:07 -0800

    Initial Commit Part 2

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # yupi :)
 import smtplib
 from email.message import EmailMessage
+from hidden import password, sender, reciever
 
 # set your email and password
 # please use App Password
-email_address = "aidenfavish@gmail.com"
-email_password = "GE9KdsVDNxH3kySg"
+email_address = sender
+email_password = password
 
 # create email
 msg = EmailMessage()
 msg['Subject'] = "Aquaphonics Garden Alert"
 msg['From'] = email_address
-msg['To'] = "8582659484@txt.att.net"
+msg['To'] = reciever
 msg.set_content("System has detected a leak and both motors have been shut off")
 
 # send email
