bd2e60cb4db42fe4b999296ab2d5fd21baed8855
Aiden F <aidenfavish@gmail.com>
2022-12-09 07:14:32 -0800

    Addded internet checker and version 1 of multiple recipient capability

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-# yupi :)
 import smtplib
+import socket
 from email.message import EmailMessage
-from hidden import password, sender, reciever
+from hidden import password, sender, reciever, reciever2
+from time import sleep
 
-# set your email and password
-# please use App Password
-#turn into array
+# I know I misspelled reciever in my variables :/
 email_address = sender
 email_password = password
 
-# create email
-msg = EmailMessage()
-msg['Subject'] = "Aquaphonics Garden Alert"
-msg['From'] = email_address
-msg['To'] = reciever
-msg.set_content("Aquaphonics System has detected a leak and both motors have been shut off")
+# changes array of emails to format that EmailMessage allows
+reciever3 = ""
+for i in range(0, len(reciever2)):
+    if i == 0:
+        reciever3 += reciever2[i]
+    else:
+        reciever3 += ", " + reciever2[i]
+print("receiver 3 = " + reciever3)
 
-# send email
-with smtplib.SMTP_SSL('smtp-relay.sendinblue.com', 465) as smtp:
-    smtp.login(email_address, email_password)
-    smtp.send_message(msg)
+# Constructs the email
+msg = EmailMessage()
+msg['Subject'] = "Aqua-phonics Garden Alert"
+msg['From'] = email_address
+msg['To'] = reciever3
+msg.set_content("System has detected a leak and both motors have been shut off")
+
+# Checks for internet then sends when it can, or else it will continuously check every 5 seconds
+internet = False
+while (internet != True):
+    try:
+        # send email
+        with smtplib.SMTP_SSL('smtp-relay.sendinblue.com', 465) as smtp:
+            smtp.login(email_address, email_password)
+            smtp.send_message(msg)
+        internet = True
+    except socket.gaierror as err:
+        print(err)
+        sleep(5)
+
+print("Complete")
